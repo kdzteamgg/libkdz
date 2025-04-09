42a97c98f233dfd39018280488bfefc6549850e4
zzz <118587387+kdzteamgg@users.noreply.github.com>
2025-04-09 10:28:28 +0700

    Update simpleui.lua

--- simpleui.lua
+++ simpleui.lua
@@ -1,4 +1,4 @@
--- cc
+-- Simple
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
